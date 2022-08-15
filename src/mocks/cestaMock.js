b20e0c215c9cca01954da54f847e173da02fe2da
Gustavobtfelix <90421959+Gustavobtfelix@users.noreply.github.com>
2022-08-15 16:19:49 -0300

    adicionando imagens no mock

--- src/mocks/cestaMock.js
+++ src/mocks/cestaMock.js
@@ -1,5 +1,10 @@
-import logoFazenda from '../../assets/logoFazenda.png';
 //mocks são informações ou funções feitas para substituir suas originais
+import logoFazenda from '../../assets/logoFazenda.png';
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abóbora.png';
 
 const cesta = {
     topo: {
@@ -12,6 +17,31 @@ const cesta = {
         descricaoTexto: "Uma cesta com produtos selecionados cuidadosamente cuidadosamente da fazenda direto para sua cozinha",
         precoTexto: "R$ 40,00",
     },
+    itens: {
+        titulo: "Itens da cesta",
+        lista: [
+          {
+            nome: "Tomate",
+            imagem: tomate,
+          },
+          {
+            nome: "Brócolis",
+            imagem: brocolis,
+          },
+          {
+            nome: "Batata",
+            imagem: batata,
+          },
+          {
+            nome: "Pepino",
+            imagem: pepino,
+          },
+          {
+            nome: "Abóbora",
+            imagem: abobora,
+          }
+        ]
+      }
 }
 
 export default cesta;
